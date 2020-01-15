f5d00ddf9fc35ccbb638aff06d63156957d49e53
nastinsk <nastinsk@gmail.com>
2020-01-15 15:30:35 -0800

    some css added, all connected, one test added

--- store_app/tests.py
+++ store_app/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class StoreTests(TestCase):
+
+    def setUp(self):
+        self.manufacturer = get_user_model().objects.create_user(username = "test_user", email = 'test_user@gmail.com', password="password")
+
+        self.product = Product.objects.create(title = "piroshky", description = "loremipsum text lalala", manufacturer = self.manufacturer)
+
+    def test_string_representation(self):
+
+        product = Product.objects.get(id=1)
+
+        self.assertEqual(str(product), 'piroshky' )
+
--- store_app/views.py
+++ store_app/views.py
@@ -1,7 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 class HomePageView(ListView):
     template_name = 'home.html'
     model = Product
+    context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    template_name = 'product_detail.html'
+    model = Product
+
 
